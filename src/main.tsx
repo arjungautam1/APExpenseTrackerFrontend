efd9c3356a5cabd873ad2da8bdd5fe3fdd0e9c99
arjungautam1 <arjungautam5431@gmail.com>
2025-08-16 17:32:47 -0400

    Fix asset paths for current subpath deployment

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/apexpensetrackerfrontend2">
       <App />
       <Toaster 
         position="top-right"
